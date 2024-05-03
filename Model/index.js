9e2f0c6a8a5a4c8f075d7d6adff7fc73864c7ee9
unknown <Nikhitha.Jayarapu@heits.in>
2024-05-03 14:46:00 +0530

    first commit

---
+++ Model/index.js
@@ -0,0 +1,13 @@
+const{Sequelize,DataTypes}=require('sequelize');
+const sequelize=new Sequelize('postgres://postgres:chinnu@localhost:5432/project',{dialect:"postgres"})
+sequelize.authenticate().then(()=>{
+    console.log('Database connected to project')
+}).catch((err)=>{
+console.log(err)
+})
+const db={}
+db.Sequelize=Sequelize
+db.sequelize=sequelize
+
+db.users=require('./userModel')(sequelize,DataTypes)
+module.exports=db
